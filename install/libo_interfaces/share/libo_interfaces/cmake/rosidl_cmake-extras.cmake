78670bdae62cb4ddd64ec8e28c85671ce329ad99
dan <kimdaren2@gmail.com>
2025-07-25 05:05:01 +0900

    [main] robot_commander - topic subscriber added and checked it's working

--- install/libo_interfaces/share/libo_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(libo_interfaces_IDL_FILES "msg/BookInfo.idl;srv/BookSearch.idl;srv/Navigate.idl")
-set(libo_interfaces_INTERFACE_FILES "msg/BookInfo.msg;srv/BookSearch.srv;srv/Navigate.srv")
--- src/main_server/main_server/services/robot_commander.py
+++ src/main_server/main_server/services/robot_commander.py
@@ -4,7 +4,8 @@ import rclpy # ROS 2 파이썬 클라이언트 라이브러리
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped # 메시지 타입 임포트
 from rclpy.duration import Duration # 시간 관련 클래스
-from libo_interfaces.srv import Navigate # 우리가 만든 서비스 임포트
+from libo_interfaces.srv import Navigate as NavigateSrv # 서비스 임포트
+from libo_interfaces.msg import GoalPose # 이름이 변경된 GoalPose 메시지를 임포트
 import math  # 수학 함수 사용을 위한 모듈
 import tf_transformations  # 오일러 → 쿼터니언 변환용 모듈
 import threading # 스레딩 라이브러리 임포트
@@ -36,7 +37,15 @@ class RobotCommander(BasicNavigator): # Node 대신 BasicNavigator를 직접 상
         )
         
         # 'navigate' 라는 이름으로 Navigate 서비스를 생성
-        self.srv = self.create_service(Navigate, 'navigate', self.navigate_callback)
+        self.srv = self.create_service(NavigateSrv, 'navigate', self.navigate_callback)
+
+        # '/goal_pose' 토픽을 구독하는 subscriber 생성
+        self.subscription = self.create_subscription(
+            GoalPose,
+            '/goal_pose',
+            self.goal_pose_callback,
+            10)
+        self.get_logger().info("'/goal_pose' 토픽 구독 시작")
 
     def go_to_pose(self, x, y, yaw_degrees): # 로봇을 움직이게 하는 코드!
         """지정한 좌표로 로봇을 이동시키고, 완료될 때까지 결과를 모니터링하는 함수."""
@@ -108,6 +117,20 @@ class RobotCommander(BasicNavigator): # Node 대신 BasicNavigator를 직접 상
         response.success = True
         response.message = "Navigation 명령 잘 받았고 시작됨!"
         return response
+
+    def goal_pose_callback(self, msg): # 명령 msg 메세지 받으면 실행
+        """'/goal_pose' 토픽을 구독하여 네비게이션 명령을 처리하는 콜백 함수"""
+        # 이미 다른 네비게이션 작업이 진행 중인 경우, 새 명령을 무시해.
+        if self.is_navigating:
+            self.get_logger().warn('이미 다른 네비게이션 작업을 수행중입니다. 토픽 명령을 무시합니다.')
+            return
+
+        self.get_logger().info(f"토픽으로 명령 받음: X={msg.x}, Y={msg.y}, Yaw={msg.yaw}")
+
+        # go_to_pose 함수는 완료될 때까지 시간이 걸리므로(blocking),
+        # 별도의 스레드에서 실행해.
+        nav_thread = threading.Thread(target=self.go_to_pose, args=(msg.x, msg.y, msg.yaw))
+        nav_thread.start()
 
     def _set_initial_pose(self): #로봇의 시작 위치를 설정해줌~
         """로봇의 초기 위치(initial pose)를 설정하는 함수. RViz2에서 2D Pose Estimate를 클릭하는 것과 같다."""
