94d3d084f2121ce8e65f57d30883c9f534678a79
dan <kimdaren2@gmail.com>
2025-07-25 15:01:51 +0900

    [system] directory structure overhauled

--- ros_ws/src/admin/setup.py
+++ ros_ws/src/admin/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'ui'), glob('admin/*.ui')), # UI 파일을 설치하도록 추가
+        (os.path.join('share', package_name, 'ui'), glob('ui/*.ui')), # UI 파일을 설치하도록 추가
     ],
     install_requires=['setuptools'],
     zip_safe=True,
