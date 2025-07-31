d57f2109dea2ef53f65ac90d3de4b84c8c1d00c9
이건우 <kwoo230@gmail.com>
2025-07-31 14:15:53 +0900

    libo navigator update
    added service msg server
    fixed initial pose  error

---
+++ ros_ws/src/libo_navigator/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'libo_navigator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nmd',
+    maintainer_email='kwoo230@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'navigator_node = libo_navigator.navigator_node:main',
+
+        ],
+    },
+)
+
