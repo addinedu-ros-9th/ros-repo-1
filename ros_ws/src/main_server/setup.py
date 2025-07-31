2a5eb80ce75282398bdd176a1acdf2b7cb1b3b93
김민수 <ajferhcz153@gmail.com>
2025-07-31 19:15:26 +0900

    [camera_monitoring] main_server camera mornitoring file uploaded

--- ros_ws/src/main_server/setup.py
+++ ros_ws/src/main_server/setup.py
@@ -17,13 +17,13 @@ setup(
     maintainer_email='leesh20806@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'book_search_service = main_server.services.book_search_service:main',
             'robot_commander = main_server.services.robot_commander:main',
             'aladin_book_register = main_server.services.aladin_book_register:main',
             'task_manager = main_server.services.task_manager:main',
+            'camera_monitoring = main_server.camera_monitoring:main',
         ],
     },
 )
